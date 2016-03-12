d1a29c13f072cfeef11b40a3d3d3f189f381f2b7
barooahn <barooahn@gmail.com>
2016-03-12 14:43:29 -0800

    added google analytics

--- resources/views/layouts/home.blade.php
+++ resources/views/layouts/home.blade.php
@@ -35,6 +35,5 @@
 
             <!-- Go to www.addthis.com/dashboard to customize your tools -->
     <script type="text/javascript" src="//s7.addthis.com/js/300/addthis_widget.js#pubid=ra-56a36cca3e6619f4" async="async"></script>
-    <script src="{{ asset('js/site.js') }}"></script>
 </body>
 </html>
